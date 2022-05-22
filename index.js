04a5b3b0003e4459c73b44671b890676744be190
Kate Danilova <ecdanilova@gmail.com>
2022-05-21 20:33:23 -0400

    create files

---
+++ index.js
@@ -0,0 +1,24 @@
+const back = document.querySelector('#back');
+const next = document.querySelector('#next');
+
+const photos = ['images/01.jpeg', 'images/02.jpeg', 'images/03.jpeg', 'images/04.jpeg', 'images/05.jpeg'];
+
+//console.log(photos[2]);
+
+let i=0;
+
+next.addEventListener('click', () => {
+i++;
+if (i > photos.length - 1) {
+    i=0;
+}
+document.querySelector('#pictures').src = photos[i];
+})
+
+back.addEventListener('click', () => {
+i--;
+if (i < 0) {
+    i = photos.length - 1;
+}
+document.querySelector('#pictures').src = photos[i];
+})
